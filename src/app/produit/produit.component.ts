05a487e0f0fe9958a7082c5daba30c67bb7434b2
abdo <abdo.sybari@gmail.com>
2019-12-09 22:22:51 +0100

    l'ajout et la modification dans package.json

---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import {ProduitMockService } from './produit.mock.service';
+import { Produit } from '../shared/Produit';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent implements OnInit {
+
+  produits: Produit[];
+  constructor(private produitservice: ProduitMockService) { }
+
+  ngOnInit() {
+    this.produits = this.produitservice.getProduits();
+  }
+
+}
