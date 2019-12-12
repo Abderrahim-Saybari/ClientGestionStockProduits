b6a98cb33ffb6ae681261b9df930362741484074
abdo <abdo.sybari@gmail.com>
2019-12-12 19:48:05 +0100

    amélioration d'opération de crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProduitComponent } from './produit/produit.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProduitResolver } from './produit/produit.resolver';
 
 
 const routes: Routes = [
   { path: '', redirectTo : '/dashboard', pathMatch : 'full' },
-  {path: 'produit', component: ProduitComponent},
+  {path: 'produit', component: ProduitComponent,
+      resolve: {
+        produits: ProduitResolver
+      }
+    },
   {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {enableTracing: true})],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [ProduitResolver]
 })
 export class AppRoutingModule { }
--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-import {ProduitMockService } from './produit.mock.service';
+import {ProduitService } from './produit.service';
 
 import { Produit } from '../shared/produit';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -14,17 +15,64 @@ import { Produit } from '../shared/produit';
 export class ProduitComponent implements OnInit {
 
   produits: Produit[];
+
   produitForm: FormGroup;
-  constructor(private produitservice: ProduitMockService, private fb: FormBuilder) {
+
+  operation = 'add';
+
+  selectedProduit: Produit;
+
+  constructor(private produitservice: ProduitService, private fb: FormBuilder, private route: ActivatedRoute) {
+    this.creatForm();
+   }
+
+  ngOnInit() {
+    this.initProduit();
+    this.produits = this.route.snapshot.data.produits;
+
+  }
+  loadProduits() {
+    this.produitservice.getProduits().subscribe(
+      data => {this.produits = data; },
+      err  => { console.log('An error was occured.'); },
+      () => { console.log('loading data was done'); }
+    );
+  }
+  creatForm() {
     this.produitForm = this.fb.group({
       ref: ['', Validators.required],
       quantite: ['', Validators.required],
       prixUnitaire: ['', Validators.required]
     });
-   }
-
-  ngOnInit() {
-    this.produits = this.produitservice.getProduits();
+  }
+  addProduit() {
+    const p = this.produitForm.value;
+    this.produitservice.addProduit(p).subscribe(
+      res => {
+        this.initProduit();
+        this.loadProduits();
+      }
+    );
+  }
+  updateProduit() {
+    this.produitservice.updateProduit(this.selectedProduit).subscribe(
+      res => {
+        this.initProduit();
+        this.loadProduits();
+      }
+    );
+  }
+  deleteProduit() {
+    this.produitservice.deleteProduit(this.selectedProduit.ref).subscribe(
+      res => {
+        this.selectedProduit = new Produit();
+        this.loadProduits();
+      }
+    );
+  }
+  initProduit() {
+    this.selectedProduit = new Produit();
+    this.creatForm();
   }
 
 }
